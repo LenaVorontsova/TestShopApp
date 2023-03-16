9deefc5ea9c3f6cdbfad8f9dac0472c2c3a0055e
Elena Vorontsova <lena.voroncova26@yandex.ru>
2023-03-16 09:53:59 +0400

    add: add the first model User

---
+++ TestShopApp/TestShopApp/Models/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  TestShopApp
+//
+//  Created by Lena Vorontsova on 16.03.2023.
+//
+
+import Foundation
+
+struct User {
+    let firstName: String?
+    let secondName: String?
+    let email: String?
+    let password: String?
+}
+
+extension User {
+    static var users = [
+        User(firstName: "Lena",
+             secondName: "Vorontsova",
+             email: "test@email.com",
+             password: "1234")
+    ]
+}
